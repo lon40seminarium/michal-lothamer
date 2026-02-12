333844380b6ca164f085250607d402b4db85ccd3
lon40seminarium <lon40seminarium@int.pl>
2026-02-12 15:48:59 +0100

    Create funkcje.js

---
+++ funkcje.js
@@ -0,0 +1,7 @@
+function obliczPr(){
+    let a = document.getElementById('pr_a').value;
+    let b = document.getElementById('pr_b').value;
+    let pole = a*b;
+    console.log("Pole = "+pole);
+}
+
